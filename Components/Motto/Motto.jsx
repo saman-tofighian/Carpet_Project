78a83432a78dff220c4e2f759b0ee329b1a9ba34
Saman Tofighian <samantofighian78@gmail.com>
2025-04-12 20:38:43 +0330

    responsive Motto section

--- Components/Motto/Motto.jsx
+++ Components/Motto/Motto.jsx
@@ -1,13 +1,13 @@
-import MottoLeft from "./MottoLeft";
-import MottoRight from "./MottoRight";
+import MottoLeft from './MottoLeft';
+import MottoRight from './MottoRight';
 
 export default function Motto() {
   return (
-    <section className="w-full mt-24">
-        <div className="w-full px-[6%] grid grid-cols-12">
-            <MottoRight />
-            <MottoLeft />
-        </div>
+    <section className='w-full mt-24'>
+      <div className='w-full px-[6%] grid grid-cols-12 lg:gap-x-12 xl:gap-x-0 items-center'>
+        <MottoRight />
+        <MottoLeft />
+      </div>
     </section>
-  )
+  );
 }
--- Components/Motto/MottoLeft.jsx
+++ Components/Motto/MottoLeft.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
 export default function MottoLeft() {
   return (
-    <div className='col-span-7'>
-        <figure className='w-full flex justify-end xl:pl-16 h-[450px]'>
-            <img src="./carpet.png" alt="carpet" />
-        </figure>
+    <div className='col-span-12 lg:col-span-6 xl:col-span-7 order-1 lg:order-2 pr-10 lg:pr-0'>
+      <figure className='w-full flex justify-center lg:justify-end xl:pl-16 h-[450px]'>
+        <img
+          src='./carpet.png'
+          alt='carpet'
+          className='w-[328px] h-[434px] md:w-[527px] md:h-[470px]'
+        />
+      </figure>
     </div>
-  )
+  );
 }
