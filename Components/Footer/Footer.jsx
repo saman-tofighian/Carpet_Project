2b613a13672fe8ebea2aac7f96f639e76c94807d
Saman Tofighian <samantofighian78@gmail.com>
2025-05-01 19:01:56 +0330

    update footer

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -2,76 +2,85 @@ import Link from 'next/link';
 import { FaGithub, FaLinkedin } from 'react-icons/fa6';
 export default function Footer() {
   return (
-    <footer className='w-full bg-[#F9F9F9] mt-32'>
-      <section className='w-full px-[6%] py-7 grid grid-cols-12 gap-y-12 xl:gap-x-16 xl:gap-y-0'>
-        <div className='col-span-12 xl:col-span-5 order-4 xl:order-1'>
-          <h3 className='text-[#CB1B1B] font-bold text-2xl'>ایـــرانی فرش</h3>
-          <p className='text-[16px] font-medium mt-5 leading-9'>
-            با ایرانی فرش همراه باشید و از زیبایی و شگفتی های فرش های متنوع و با
-            کیفیت ما لذت ببرید. خرید آسان، تحویل سریع و خدمات پس از فروش حرفه
-            ای، تنها چندی از ویژگی های خرید از ماست. با ایرانی فرش یک خرید آسان
-            و لذت بخش را تجربه کنید.
-          </p>
-        </div>
-        <div className='col-span-6 sm:col-span-4 xl:col-span-2 order-1 xl:order-2'>
-          <ul className='w-full text-[#505050] text-[16px] flex flex-col'>
-            <h6 className='font-bold'>فرش های دست بافت</h6>
-            <span className='texy-[#717171] mt-7 font-medium'>لوکس</span>
-            <span className='texy-[#717171] mt-5 font-medium'>قدیمی</span>
-            <span className='texy-[#717171] mt-5 font-medium'>مدرن</span>
-          </ul>
-        </div>
-        <div className='col-span-6 sm:col-span-4 xl:col-span-2 order-2 xl:order-3'>
-          <ul className='w-full text-[#505050] text-[16px] flex flex-col'>
-            <h6 className='font-bold'>فرش های ماشینی</h6>
-            <span className='texy-[#717171] mt-7 font-medium'>لوکس</span>
-            <span className='texy-[#717171] mt-5 font-medium'>قدیمی</span>
-            <span className='texy-[#717171] mt-5 font-medium'>مدرن</span>
-          </ul>
-        </div>
-        <div className='col-span-6 sm:col-span-4 xl:col-span-2 order-3 xl:order-4'>
-          <ul className='w-full text-[#505050] text-[16px] flex flex-col'>
-            <h6 className='font-bold'>سایر فرش ها</h6>
-            <span className='texy-[#717171] mt-7 font-medium'>لوکس</span>
-            <span className='texy-[#717171] mt-5 font-medium'>قدیمی</span>
-            <span className='texy-[#717171] mt-5 font-medium'>مدرن</span>
-          </ul>
-        </div>
-        <div className='col-span-12 xl:col-span-4 mt-8 order-5'>
-          <div className='w-full flex justify-around xl:justify-between'>
-            <div className='flex flex-col'>
-              <span className='font-medium text-[18px] md:text-[20px] text-[#505050]'>
-                تلفن پشتیبانی : 0212544
-              </span>
-              <div className='flex gap-x-6 mt-7'>
-                <Link
-                  href='https://www.linkedin.com/in/saman-tofighian/'
-                  target='_blank'
-                  className='text-blue-500'
-                >
-                  <FaLinkedin size='2rem' />
-                </Link>
-                <Link
-                  href='https://github.com/saman-tofighian'
-                  target='_blank'
-                  className='text-black'
-                >
-                  <FaGithub size='2rem' />
-                </Link>
-              </div>
-            </div>
-            <figure>
-              <img src='./en.png' alt='enamad' />
-            </figure>
+    <>
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+        <path
+          fill='#f9f9f9'
+          fill-opacity='1'
+          d='M0,128L34.3,149.3C68.6,171,137,213,206,224C274.3,235,343,213,411,181.3C480,149,549,107,617,74.7C685.7,43,754,21,823,42.7C891.4,64,960,128,1029,176C1097.1,224,1166,256,1234,229.3C1302.9,203,1371,117,1406,74.7L1440,32L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z'
+        ></path>
+      </svg>
+      <footer className='w-full bg-[#F9F9F9]'>
+        <section className='w-full px-[6%] py-7 grid grid-cols-12 gap-y-12 xl:gap-x-16 xl:gap-y-0'>
+          <div className='col-span-12 xl:col-span-5 order-4 xl:order-1'>
+            <h3 className='text-[#CB1B1B] font-bold text-2xl'>ایـــرانی فرش</h3>
+            <p className='text-[16px] font-medium mt-5 leading-9'>
+              با ایرانی فرش همراه باشید و از زیبایی و شگفتی های فرش های متنوع و
+              با کیفیت ما لذت ببرید. خرید آسان، تحویل سریع و خدمات پس از فروش
+              حرفه ای، تنها چندی از ویژگی های خرید از ماست. با ایرانی فرش یک
+              خرید آسان و لذت بخش را تجربه کنید.
+            </p>
           </div>
-        </div>
-        <div className='col-span-12 flex justify-center mt-8 order-6'>
-          <h3 className='text-[#505050] text-[1.2rem]'>
-            ساخته شده توسط{' '}
-            <strong className='text-[#CB1B1B]'>سامان توفیقیان</strong>
-          </h3>
-        </div>
-      </section>
-    </footer>
+          <div className='col-span-6 sm:col-span-4 xl:col-span-2 order-1 xl:order-2'>
+            <ul className='w-full text-[#505050] text-[16px] flex flex-col'>
+              <h6 className='font-bold'>فرش های دست بافت</h6>
+              <span className='texy-[#717171] mt-7 font-medium'>لوکس</span>
+              <span className='texy-[#717171] mt-5 font-medium'>قدیمی</span>
+              <span className='texy-[#717171] mt-5 font-medium'>مدرن</span>
+            </ul>
+          </div>
+          <div className='col-span-6 sm:col-span-4 xl:col-span-2 order-2 xl:order-3'>
+            <ul className='w-full text-[#505050] text-[16px] flex flex-col'>
+              <h6 className='font-bold'>فرش های ماشینی</h6>
+              <span className='texy-[#717171] mt-7 font-medium'>لوکس</span>
+              <span className='texy-[#717171] mt-5 font-medium'>قدیمی</span>
+              <span className='texy-[#717171] mt-5 font-medium'>مدرن</span>
+            </ul>
+          </div>
+          <div className='col-span-6 sm:col-span-4 xl:col-span-2 order-3 xl:order-4'>
+            <ul className='w-full text-[#505050] text-[16px] flex flex-col'>
+              <h6 className='font-bold'>سایر فرش ها</h6>
+              <span className='texy-[#717171] mt-7 font-medium'>لوکس</span>
+              <span className='texy-[#717171] mt-5 font-medium'>قدیمی</span>
+              <span className='texy-[#717171] mt-5 font-medium'>مدرن</span>
+            </ul>
+          </div>
+          <div className='col-span-12 xl:col-span-4 mt-8 order-5'>
+            <div className='w-full flex justify-around xl:justify-between'>
+              <div className='flex flex-col'>
+                <span className='font-medium text-[18px] md:text-[20px] text-[#505050]'>
+                  تلفن پشتیبانی : 0212544
+                </span>
+                <div className='flex gap-x-6 mt-7'>
+                  <Link
+                    href='https://www.linkedin.com/in/saman-tofighian/'
+                    target='_blank'
+                    className='text-blue-500'
+                  >
+                    <FaLinkedin size='2rem' />
+                  </Link>
+                  <Link
+                    href='https://github.com/saman-tofighian'
+                    target='_blank'
+                    className='text-black'
+                  >
+                    <FaGithub size='2rem' />
+                  </Link>
+                </div>
+              </div>
+              <figure>
+                <img src='./en.png' alt='enamad' />
+              </figure>
+            </div>
+          </div>
+          <div className='col-span-12 flex justify-center mt-8 order-6'>
+            <h3 className='text-[#505050] text-[1.2rem]'>
+              ساخته شده توسط{' '}
+              <strong className='text-[#CB1B1B]'>سامان توفیقیان</strong>
+            </h3>
+          </div>
+        </section>
+      </footer>
+    </>
   );
 }
